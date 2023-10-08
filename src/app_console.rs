ce7b363ddeaf9886fa01d5171081845a894418ba
Fabio Murer <murer.fabio23@gmail.com>
2023-10-08 13:38:07 +0200

    lil

--- src/app_console.rs
+++ src/app_console.rs
@@ -2,6 +2,7 @@ use crate::board::*;
 use crate::move_engine::*;
 
 use clap::*;
+use std::io;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -15,6 +16,8 @@ struct Cli {
 enum Commands {
     /// Compute the best move
     Bestmove(EngineArgs),
+    /// Play a game against the cpu in the terminal
+    Play,
 }
 
 #[derive(Args)]
@@ -42,35 +45,38 @@ pub fn app() {
             let m = e.iterative_depening(&b);
             println!("{:?}", m);
         }
-    }
-    /*let mut board = board::Board::new();
-    let mut e = move_engine::Engine::new(3, 100_000);
+        Commands::Play => {
+            let mut board = Board::new();
+            let mut e = Engine::new(3, 100_000);
 
-    println!("p1 for first player, p2 for second");
-    let mut buf = String::new();
-    io::stdin().read_line(&mut buf).unwrap();
-
-    let human = match buf.trim() {
-        "p1" => board::Player::P1,
-        "p2" => board::Player::P2,
-        _ => panic!("player option not valid"),
-    };
-
-    while board.gamestate() == board::GameState::Open {
-        if board.player() == human {
+            println!("p1 for first player, p2 for second");
             let mut buf = String::new();
             io::stdin().read_line(&mut buf).unwrap();
-            let col: u8 = buf.trim().parse().unwrap();
 
-            board.make_move(col);
-        } else {
-            let m = e.iterative_depening(&board);
-            board.make_move(m.col());
-            println!("{:?}", m);
+            let human = match buf.trim() {
+                "p1" => Player::P1,
+                "p2" => Player::P2,
+                _ => panic!("player option not valid"),
+            };
+
+            while board.gamestate() == GameState::Open {
+                if board.player() == human {
+                    let mut buf = String::new();
+                    io::stdin().read_line(&mut buf).unwrap();
+                    let col: u8 = buf.trim().parse().unwrap();
+
+                    board.make_move(col);
+                } else {
+                    let m = e.iterative_depening(&board);
+                    board.make_move(m.col());
+                    println!("{:?}", m);
+                }
+
+                board.bitboard().print();
+            }
+            println!("{:?}", board.gamestate());
         }
-
-        board.bitboard().print();
     }
-    println!("{:?}", board.gamestate());
-    */
+    /*
+     */
 }
